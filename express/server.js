6428fae619642f5b9c71be6f3c4af7e4dcd1c356
ankitgupta1994 <43522911+ankitgupta1994@users.noreply.github.com>
2021-05-30 15:54:22 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -5,18 +5,13 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// use the express-static middleware
+app.use(express.static("public"))
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// define the first route
+app.get("../public/authentication.js", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 
 module.exports = app;
 module.exports.handler = serverless(app);
